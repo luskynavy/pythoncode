0db3ffbe6e35d91aeec92f29c45b305d86b6cbdc
LuskyWork <Lusky@work.com>
2014-05-14 15:45:27 +0200

    FTPMirror 14-05-14 Loading ok and fixed line size when many lines in list

--- ftpmirror/main.py
+++ ftpmirror/main.py
@@ -9,8 +9,8 @@ import os
 from ftplib import FTP
 from datetime import datetime
 
-localPath = "gen"
-remotePath = "/syncback/gen"
+#localPath = "gen"
+#remotePath = "/syncback/gen"
 
 #localPath = "D:\Data\scummvm-1.6.0-win32\Saves"
 #remotePath = "/syncback/scummvm saves"
@@ -19,21 +19,29 @@ remotePath = "/syncback/gen"
 #remotePath = "/syncback/PPSSPP SAVEDATA/ULES01431GAMEDATA"
 
 class FTPView(GridLayout):
-    def __init__(self, **kwargs):        
+    def __init__(self, **kwargs):
+        self.listftp = []
         
-        self.ScanFTP()        
+        self.first_time = 1        
         
         kwargs['cols'] = 1
         super(FTPView, self).__init__(**kwargs)
         
+        '''self.localPath = "gen"
+        self.remotePath = "/syncback/gen"
+        self.ScanFTP()
+        self.BuildList()'''
+        
+        self.Load()
+    
+    def BuildList(self):
         args_converter = \
             lambda row_index, rec: \
                 {'text': rec['text'],
-                 #'size_hint_x': .3,
-                 #'width' : 800,
-                 'height': 25,
+                 'height': 25,                 
+                 'size_hint_y': None,
                  'cls_dicts': [{'cls': ListItemLabel,
-                        'kwargs': {'text': rec['text'][3]}},
+                        'kwargs': {'text': rec['text'][3], 'height': 25,}},
                        {'cls': ListItemLabel,
                         'kwargs': {'text': rec['text'][0], 'size_hint_x':.3}},
                        {'cls': ListItemLabel,
@@ -42,7 +50,7 @@ class FTPView(GridLayout):
                         'kwargs': {'text': rec['text'][2], 'size_hint_x':.3}}]}
 
         item_strings = [self.listftp[index][3] for index in xrange(len(self.listftp))]
-        integers_dict =         { self.listftp[i][3]: {'text': self.listftp[i], 'is_selected': False} for i in xrange(len(self.listftp))}
+        integers_dict = { self.listftp[i][3]: {'text': self.listftp[i], 'is_selected': False} for i in xrange(len(self.listftp))}
 
 
         dict_adapter = DictAdapter(#sorted_keys=item_strings,
@@ -59,13 +67,13 @@ class FTPView(GridLayout):
         #self.list_view = ListView(item_strings = self.listftp)
        
         self.buttonLoad = Button(text='Load', size_hint_y = .1)
-        self.buttonLoad.bind(on_release=self.Load)
+        self.buttonLoad.bind(on_release = self.Load)
         
-        self.buttonDoIt = Button(text='Do it', size_hint_y = .1)
-        self.buttonDoIt.bind(on_release=self.DoIt)
+        self.buttonDoIt = Button(text = 'Do it', size_hint_y = .1)
+        self.buttonDoIt.bind(on_release = self.DoIt)
         
-        self.buttonCancel = Button(text='Cancel', size_hint_y = .1)#, center_x = 150)
-        self.buttonCancel.bind(on_release=self.Cancel)
+        self.buttonCancel = Button(text = 'Cancel', size_hint_y = .1)#, center_x = 150)
+        self.buttonCancel.bind(on_release = self.Cancel)
 
         self.add_widget(self.buttonLoad)
         self.add_widget(self.list_view)
@@ -83,19 +91,19 @@ class FTPView(GridLayout):
 
         ftp = FTP("ftpperso.free.fr", user, mdp)
 
-        ftp.cwd(remotePath)
+        ftp.cwd(self.remotePath)
 
         remoteDir = ftp.nlst()
         print "remote: ", remoteDir
 
-        localDir = os.listdir(localPath)
+        localDir = os.listdir(self.localPath)
         print " local", localDir
 
         print
 
         #check if files must be uploaded or downloaded
         for localFile in localDir:
-            localTime = os.path.getmtime(os.path.join(localPath, localFile))
+            localTime = os.path.getmtime(os.path.join(self.localPath, localFile))
             localTime = datetime.utcfromtimestamp(localTime) #syncback seems to work in utc
             #localTime = datetime.fromtimestamp(localTime)
                 
@@ -113,13 +121,13 @@ class FTPView(GridLayout):
                 remoteTime = datetime.strptime(remoteTime[4:], "%Y%m%d%H%M%S")                
                 
                 if localTime > remoteTime:
-                    print "local is newer than remote :", localTime, remoteTime # must upload local then touch local file to synchronize time
+                    print "local ", localFile, " is newer than remote :", localTime, remoteTime # must upload local then touch local file to synchronize time
                     self.listftp.append([str(localTime), ">", str(remoteTime), localFile])
                 elif localTime == remoteTime:
-                    print "local is same than remote :", localTime, remoteTime #nothing to do
+                    print "local ", localFile, " is same than remote :", localTime, remoteTime #nothing to do
                     self.listftp.append([str(localTime), "=", str(remoteTime), localFile])
                 else:
-                    print "local is older than remote :", localTime, remoteTime #must download remote then try to set local time with remote time 
+                    print "local ", localFile, " is older than remote :", localTime, remoteTime #must download remote then try to set local time with remote time 
                     self.listftp.append([str(localTime), "<", str(remoteTime), localFile])
             else:
                 print localFile, " is not present on remote" #must upload local
@@ -142,14 +150,30 @@ class FTPView(GridLayout):
     def Load(self, *l):
         print "Load"
         
-        self.fl = FileChooserListView(path = ".", rootpath = ".", filters = ["*.*"],
-                 dirselect=False, size=(400,400), center_x = 250, center_y = 250)
-        self.fl.bind(selection=self.on_selected)
+        self.fl = FileChooserListView(path = ".", rootpath = ".", filters = ["*.ftp"],
+                 dirselect=False)
+        self.fl.bind(selection = self.on_selected)
         self.add_widget(self.fl)
     
     def on_selected(self, filechooser, selection):
         print 'on_selected', selection
         self.remove_widget(self.fl)
+        
+        if self.first_time != 1:
+            self.remove_widget(self.buttonLoad)
+            self.remove_widget(self.list_view)
+            self.remove_widget(self.buttonDoIt)
+            self.remove_widget(self.buttonCancel)
+            
+        self.first_time = 0
+            
+        fileRead = open(selection[0], "r")
+        self.localPath = fileRead.readline().rstrip('\n')
+        self.remotePath = fileRead.readline().rstrip('\n')
+        fileRead.close()
+        self.ScanFTP()
+        
+        self.BuildList()
         
     def DoIt(self, *l):
         print "DoIt"
@@ -163,4 +187,4 @@ class FTPView(GridLayout):
 
 if __name__ == '__main__':
     from kivy.base import runTouchApp
-    runTouchApp(FTPView(width=1280))
+    runTouchApp(FTPView(width = 1280))
